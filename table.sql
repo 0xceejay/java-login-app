78441eaeb081f9b73ca9b23c64f114706699e42b
0xceejay <0xceejay@gmail.com>
2023-02-19 21:26:09 +0100

    Added sql script to run on startup

---
+++ table.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS UserDb;
+USE UserDB;
+
+CREATE TABLE IF NOT EXISTS Employee (
+  id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(250),
+  last_name VARCHAR(250),
+  email VARCHAR(250),
+  username VARCHAR(250),
+  password VARCHAR(250),
+  regdate VARCHAR(250),
+  PRIMARY KEY (id)
+);
